2dc74c643dc790bb6beb29f48e41f54db6976a8d
Eduardo Hernandez Soto <es50678@gmail.com>
2020-06-27 18:23:24 -0400

    make categories on user private

--- src/data-sources/models/user.ts
+++ src/data-sources/models/user.ts
@@ -4,7 +4,7 @@ import Base from './base';
 export default class User extends Base {
   name: string;
   email: string;
-  categories: [Category?] = [];
+  #categories: [Category?] = [];
 
   constructor(user) {
     super();
@@ -14,16 +14,16 @@ export default class User extends Base {
     this.email = user.email;
   }
 
-  async getCategories() {
+  async categories(): Promise<[Category?]> {
     const query = 'MATCH (user:User {id: $id})-[:OWNS]-(categories:Category) \n RETURN categories'
     const result = await this.session.run(query, { id: this.id });
     const records = result.records;
 
     records.forEach((record) => {
       const properties = record.get('categories').properties
-      this.categories.push(new Category(properties));
+      this.#categories.push(new Category(properties));
     });
 
-    return this.categories;
+    return this.#categories;
   }
 }
