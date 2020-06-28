5c7f59c7eb9e33f7fea8d033e5626a34db3c70ef
Eduardo Hernandez Soto <es50678@gmail.com>
2020-06-28 00:00:12 -0400

    add date time and time duration scalars

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,16 +1,54 @@
+import moment from 'moment';
+import { GraphQLScalarType, Kind } from 'graphql';
+
 import Category from '../data-sources/models/category';
 import User from '../data-sources/models/user';
 import { ContextInterface, UserCreationOptions as UserArgs} from '../data-sources/user-source';
 import Duration from '../data-sources/models/duration';
 
+const dateTimeScalar = new GraphQLScalarType({
+  name: 'DateTime',
+  description: 'Custom DateTime scalar type - A unix timestamp in seconds',
+  serialize(value) { // gets invoked when serializing the result to send it back to a client
+    return moment(value).unix();
+  },
+  parseValue(value) { // gets invoked to parse client input that was passed through variables
+    return new Date(value);
+  },
+  parseLiteral(ast) { // gets invoked to parse client input that was passed inline in the query
+    if (ast.kind === Kind.INT) {
+      return parseInt(ast.value, 10); // ast value is always in string format
+    }
+    return null;
+  },
+});
+
+const timeDurationScalar = new GraphQLScalarType({
+  name: 'TimeDuration',
+  description: 'Custom TimeDuration scalar type - an ISO 8601 formatted duration string',
+  serialize(value) { // gets invoked when serializing the result to send it back to a client
+    return moment.duration(value).toISOString();
+  },
+  parseValue(value) { // gets invoked to parse client input that was passed through variables
+    return moment.duration(value);
+  },
+  parseLiteral(ast) { // gets invoked to parse client input that was passed inline in the query
+    if (ast.kind === Kind.INT) {
+      return parseInt(ast.value, 10); // ast value is always in string format
+    }
+    return null;
+  },
+});
+
 const resolvers = {
+  DateTime: dateTimeScalar,
+  TimeDuration: timeDurationScalar,
   User: {
     categories(user: User): Promise<[Category?]> {
       return user.categories();
     },
-    async durations(user: User): Promise<[Duration?]> {
-      const duartions = await user.durations();
-      return duartions;
+    durations(user: User): Promise<[Duration?]> {
+      return user.durations();
     }
   },
   Mutation: {
