d8def40fc5fe8bd21d4d5eebfaef3d5a3343eecf
Eduardo Hernandez Soto <es50678@gmail.com>
2020-06-28 01:22:13 -0400

    add context function

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,16 @@ import UserSource from './data-sources/user-source';
 const server = new ApolloServer({
   resolvers,
   typeDefs,
+  async context(params) {
+    const authToken = (params.req.headers && params.req.headers.authorization) || '';
+    const email = Buffer.from(authToken, 'base64').toString();
+
+    const user = await new UserSource().findUserByEmail({ email }).catch(() => {
+      return null;
+    });
+
+    return { loggedInUser: user }
+  },
   dataSources() {
     return {
       user: new UserSource()
